d0fb97761a81be974cb043792f3fb7de62983da6
JillBirchall <jill.m.birchall@gmail.com>
2022-01-19 12:12:20 +0000

    Loader added

--- src/Loader.tsx
+++ src/Loader.tsx
@@ -2,8 +2,14 @@ import React from "react";
 
 export const Loader = () => {
   return (
-    <div>
-      <h2 className="text-center">Loading....</h2>
+    <div className="d-flex justify-content-center">
+      <div
+        className="spinner-border text-secondary"
+        style={{ width: "3rem", height: "3rem", marginTop: "10%" }}
+        role="status"
+      >
+        <span className="visually-hidden">Loading...</span>
+      </div>
     </div>
   );
 };
